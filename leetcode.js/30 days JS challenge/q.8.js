632a191607082b8d8454f564676ab68b829ff251
Your Name <ayansaifi2nd@gmail.com>
2025-07-21 20:13:19 +0530

    q.9

---
+++ leetcode.js/30 days JS challenge/q.8.js
@@ -0,0 +1,15 @@
+//Return Length of Arguments Passed
+
+//Write a function argumentsLength that returns the count of arguments passed to it. 
+
+
+ 
+var argumentsLength = function(...args) {
+    return args.length
+};
+console.log(argumentsLength(1,2,3))
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
+
+ 
