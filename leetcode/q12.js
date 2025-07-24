1c2b2df5441c12326ff3fb210b6a3cc011f5eac0
Your Name <ayansaifi2nd@gmail.com>
2025-07-24 12:42:41 +0530

    q12

---
+++ leetcode/q12.js
@@ -0,0 +1,18 @@
+//Sleep
+
+
+ 
+// Given a positive integer millis, write an asynchronous function that sleeps for millis milliseconds. It can resolve any value.
+
+// Note that minor deviation from millis in the actual sleep duration is acceptable.
+
+
+ 
+
+async function sleep(millis){
+    await new Promise(resolve => setTimeout(resolve, millis)) 
+}
+
+
+    let t = Date.now()
+    sleep(100).then(() => console.log(Date.now() - t)) // 100
