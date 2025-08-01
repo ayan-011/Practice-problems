9ae6618985f487768d3c00809b464716976904b7
Your Name <ayansaifi2nd@gmail.com>
2025-08-01 14:47:25 +0530

    solution

--- dailychallenge/2.js
+++ dailychallenge/2.js
@@ -4,3 +4,32 @@
 // Given an integer numRows, return the first numRows of Pascal's triangle.
 
 // In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+
+
+var generate = function(numRows){
+    let triangle = []
+
+    if (numRows === 0) return []
+
+    let firstRow = [1]
+    triangle.push(firstRow)
+
+    for (let i = 1; i < numRows; i++) {
+        let prevRow = triangle[i - 1]
+        let row = []
+        row.push(1)
+
+        for (let j = 1; j < i; j++) {
+            const element = prevRow[j - 1] + prevRow[j]
+            row.push(element)
+        }
+
+        row.push(1)
+        triangle.push(row)
+    }
+
+    return triangle;
+}
+
+const final = generate(5)
+console.log(final)  // output [ [ 1 ], [ 1, 1 ], [ 1, 2, 1 ], [ 1, 3, 3, 1 ], [ 1, 4, 6, 4, 1 ] ]
